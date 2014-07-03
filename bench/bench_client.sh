777f6d7548ac239bbc93adb0c37a8fe9bce078d6
Arjan Topolovec <arjan.top@gmail.com>
2014-07-03 20:56:34 +0200

    Added support for choosing request type for client benchmark

--- bench/bench_client.sh
+++ bench/bench_client.sh
@@ -8,7 +8,7 @@ if [ ! -d fixtures ]; then
     exit 1
 fi
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "Usage: $0 TRANSFER_MODE REQUEST_TYPE"
     exit 1
 fi
@@ -18,13 +18,28 @@ if [ "$1" != "octet" ] && [ "$1" != "netascii" ]; then
     exit 1
 fi
 
+if [ "$2" != "get" ] && [ "$2" != "put" ]; then
+    echo "Request time must be either 'get' or 'put'"
+    exit 1
+fi
+
 for file in `ls fixtures`
 do
-    echo "Getting file: [0;32m$file[0m"
-    echo "Using: [0;31mtftp-hpa[0m"
-    sleep 1
-    /usr/bin/time "$TFTP_BIN" -v 127.0.0.1 69 -m octet -c get "$file" /tmp/testfile
-    echo "Using: [0;34mtftp-rs[0m"
-    sleep 1
-    /usr/bin/time ../target/get "$file"
+    if [ "$2" == "get" ]; then
+        echo "Getting file: [0;32m$file[0m"
+        echo "Using: [0;31mtftp-hpa[0m"
+        sleep 1
+        /usr/bin/time "$TFTP_BIN" -v 127.0.0.1 69 -m octet -c get "$file" /tmp/testfile
+        echo "Using: [0;34mtftp-rs[0m"
+        sleep 1
+        /usr/bin/time ../target/get "$file"
+    else
+        echo "Putting file: [0;32m$file[0m"
+        echo "Using: [0;31mtftp-hpa[0m"
+        sleep 1
+        /usr/bin/time "$TFTP_BIN" -v 127.0.0.1 69 -m octet -c put "fixtures/$file" testfile
+        echo "Using: [0;34mtftp-rs[0m"
+        sleep 1
+        /usr/bin/time ../target/put "fixtures/$file"
+    fi
 done
